84c49c5bc4d948b19955570777c5b45c8b2f73be
Your Name <you@example.com>
2018-06-16 10:23:53 +0545

    some changes

---
+++ app.py
@@ -0,0 +1,16 @@
+import time
+from flask import Flask, request
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def hello():
+   print('here')
+   print(request.form)
+   print(request.get_json())
+   return 'Hello mo! I have been seen'
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
